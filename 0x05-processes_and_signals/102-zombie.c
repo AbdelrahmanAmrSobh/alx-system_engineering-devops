b56eb0d56b8c740be35d7d6da3879268473c3d6a
ABDELRAHMAN-AMRSOBH <Abdelrahmanamr.egypt@gmail.com>
2024-01-26 14:16:12 -0800

    upload files

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+/**
+ * infinite_while - Run an infinite while loop.
+ * Return: Always 0.
+*/
+
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - create 5 useless tasks
+ * Return: 0
+*/
+
+int main(void)
+{
+	int i;
+	pid_t process;
+
+	i = 0;
+	while (i < 5)
+	{
+		process = fork();
+		if (process)
+			printf("Zombie process created, PID: %i\n", process);
+		else
+			exit(0);
+		i++;
+	}
+	infinite_while();
+	return (0);
+}
