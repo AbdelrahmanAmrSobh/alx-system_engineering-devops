9d743f92049c3226539f62b5e939076a8bc2eacd
Abdelrahman Amr Sobh <AbdelrahmanAmr.Egypt@gmail.com>
2024-04-03 08:33:24 -0700

    Solve configuration management.

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Using Puppet, create a file in /tmp.
+file { 'school':
+  ensure  => file,
+  path    => '/tmp/school',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+}
