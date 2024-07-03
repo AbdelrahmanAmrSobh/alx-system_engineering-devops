d0b8664a28f1f25b5d30a1f579cfb8336ee94af6
Abdelrahman <abdelrahmanamr.egypt@gmail.com>
2024-07-02 23:59:34 -0700

    Change file to config to /etc/ssh/ssh_config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,10 @@
 # puppet ssh configuration
 exec { 'ensure no password is required':
-  path    => '/bin',
-  command => 'echo "PasswordAuthentication no" >> ~/.ssh/config',
-  unless  => 'grep -q "PasswordAuthentication no" ~/.ssh/config'
+  command => 'echo "PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  unless  => 'grep -q "PasswordAuthentication no" /etc/ssh/ssh_config'
 }
 
 exec { 'ensure to use private key school':
-  path    => '/bin',
-  command => 'echo "IdentityFile ~/.ssh/school" >> ~/.ssh/config',
-  unless  => 'grep -q "IdentityFile ~/.ssh/school" ~/.ssh/config'
+  command => 'echo "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
+  unless  => 'grep -q "IdentityFile ~/.ssh/school" /etc/ssh/ssh_config'
 }
