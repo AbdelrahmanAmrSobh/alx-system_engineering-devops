7fd1509930c2c7d906741319991fa261702a10f5
abdelrahman <abdelrahmanamr.egypt@gmail.com>
2024-07-09 14:39:56 -0700

    Solve tasks and config server for load balance and backend

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,24 @@
+# Custom HTTP header in a nginx server
+exec { 'update server':
+  command  => 'apt-get update',
+  user     => 'root',
+  provider => 'shell',
+}
+->
+package { 'download nginx':
+  ensure   => present,
+  provider => 'apt'
+}
+->
+file_line { 'add HTTP header':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'add_header X-Served-By $hostname;'
+}
+->
+service { 'start nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => Package['nginx']
+}
