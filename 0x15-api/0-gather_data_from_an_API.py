bc777a2f6de50c1e74c2cb065a4f87de41b635f1
Abdelrahman Amr <AbdelrahmanAmr.Egypt@gmail.com>
2024-07-30 01:40:58 +0300

    Fix format

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -15,6 +15,6 @@ if __name__ == "__main__":
     tasks = requests.get(urls[1], params={keys[1]: id}).json()
     completed_tasks = [task for task in tasks if task.get('completed') is True]
     print(f'Employee {employee} is done with tasks', end='')
-    print(f'{len(completed_tasks)}/{len(tasks)}):')
+    print(f'({len(completed_tasks)}/{len(tasks)}):')
     for completed_task in completed_tasks:
         print(f'\t {completed_task.get('title')}')
