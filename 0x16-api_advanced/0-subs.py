b913b68df52b134ce5c43c5083671d24b2f2d1ce
Abdelrahman Amr <AbdelrahmanAmr.Egypt@gmail.com>
2024-08-13 19:04:15 +0300

    Solve first task

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Script that is used to get data from api
+and display this data
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """َQueries the reddit api and returns the number of subscribers"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    header = {"user-agent": "Mozilla/5.0 (Windows NT 6.1; \
+              Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"}
+    response = requests.get(url=url, headers=header, allow_redirects=False)
+    if response.status_code == 200:
+        return response.json()['data']['subscribers']
+    return 0
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,4 @@
+README file for api advanced
+use requests library to query reddit api
+first task query about subreddit count of subscribers using url
+https://www.reddit.com/r/subreddit/about.json
