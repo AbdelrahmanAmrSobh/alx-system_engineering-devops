789e9c490985a573de7c2825005d6ffca06f1d1e
Abdelrahman Amr <AbdelrahmanAmr.Egypt@gmail.com>
2024-08-13 19:13:07 +0300

    Change user-agent

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,8 +9,7 @@ import requests
 def number_of_subscribers(subreddit):
     """َQueries the reddit api and returns the number of subscribers"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    header = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; \
-              Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"}
+    header = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
     response = requests.request(url=url, headers=header,
                                 allow_redirects=False, method='GET')
     if response.status_code == 200:
