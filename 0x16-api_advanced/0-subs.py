722e513d79c71578f47a5827c1f2b46629d4820e
Abdelrahman Amr <AbdelrahmanAmr.Egypt@gmail.com>
2024-08-13 19:09:40 +0300

    swap requests.get to requests.request

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,7 +11,8 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     header = {"user-agent": "Mozilla/5.0 (Windows NT 6.1; \
               Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"}
-    response = requests.get(url=url, headers=header, allow_redirects=False)
+    response = requests.request(url=url, headers=header,
+                                allow_redirects=False, method='GET')
     if response.status_code == 200:
         return response.json()['data']['subscribers']
     return 0
