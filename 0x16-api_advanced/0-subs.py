26294bf33a88593777dd2d9a4a968ddea528757d
Abdelrahman Amr <AbdelrahmanAmr.Egypt@gmail.com>
2024-08-13 19:14:51 +0300

    change f string to string formatted

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,10 +8,9 @@ import requests
 
 def number_of_subscribers(subreddit):
     """َQueries the reddit api and returns the number of subscribers"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     header = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
-    response = requests.request(url=url, headers=header,
-                                allow_redirects=False, method='GET')
+    response = requests.get(url=url, headers=header, allow_redirects=False)
     if response.status_code == 200:
         return response.json()['data']['subscribers']
     return 0
