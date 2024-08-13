cfc306e7a7381ae2966b8b966bcb21ac8db36aa9
Abdelrahman Amr <AbdelrahmanAmr.Egypt@gmail.com>
2024-08-13 19:11:06 +0300

    user-agent to User-Agent

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ import requests
 def number_of_subscribers(subreddit):
     """َQueries the reddit api and returns the number of subscribers"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    header = {"user-agent": "Mozilla/5.0 (Windows NT 6.1; \
+    header = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; \
               Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"}
     response = requests.request(url=url, headers=header,
                                 allow_redirects=False, method='GET')
